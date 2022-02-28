c4b6fbdcf28252bf6969178d07aa2fb97f1a29db
ryutaro yamashita <r-yamashita@a.3hands.co.jp>
2022-03-01 08:27:52 +0900

    test2

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,13 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+    return(
+        <>
+            <h1 className="title">{title}</h1>
+            <p>
+              <ol>
+                <li><a href="/">home</a></li>
+                <li><a href="/about">about</a></li>
+              </ol>
+            </p>
+        </>
+    );
 }
--- pages/about.js
+++ pages/about.js
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>test about</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="header about" />
         <p className="description">
-          ryus  <code>pages/index.js</code>
-          <a href="/about/">about</a>
+          ryus's about
+          <code>pages/index.js</code>
         </p>
       </main>
 
